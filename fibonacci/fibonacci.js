4397affb30a30f278aabee44b60346f56e9b5624
Aadheuss <aadheusx@gmail.com>
2023-10-28 11:15:15 +0700

    Add recursive fibonacci function

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,30 +1,28 @@
 function fibs(n) {
-  const arr = [];
+  const arr = [0, 1];
   let a = 0;
   let b = 1;
   let c = a + b;
 
-  if (n === 1) {
-    arr.push(a);
-  } else if (n === 2) {
-    arr.push(a);
-    arr.push(b);
-  } else {
-    for (let i = 1; i <= n; i++) {
-      if (i === 1) {
-        arr.push(a);
-      } else if (i === 2) {
-        arr.push(b);
-      } else {
-        arr.push(c);
-        a = b;
-        b = c;
-        c = a + b;
-      }
-    }
+  if (n === 1) return [0];
+  if (n === 2) return arr;
+  for (let i = 3; i <= n; i++) {
+    arr.push(c);
+    a = b;
+    b = c;
+    c = a + b;
   }
 
   return arr;
 }
 
 fibs(8);
+
+function fibsRec(n) {
+  if (n == 0) return [0];
+  if (n == 1) return [0, 1];
+  const arr = fibsRec(n - 1);
+  return [...arr, arr[n - 1] + arr[n - 2]];
+}
+
+fibsRec(9);
